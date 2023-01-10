1374cc5381b1557075a826d484fd098cfd3a5da4
yuna-tangent <m19068@g.metro-cit.ac.jp>
2023-01-10 13:34:51 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/hello", Hello())
+    e.GET("/api/hello", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.String(http.StatusOK, "4422: Hello!!!LoveLive! ver.2")
+    }
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4422", "message": "Help,LoveLive!"})
+    }
+}
+
